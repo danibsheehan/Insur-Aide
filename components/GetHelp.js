032ce9e9e46fbb1f21ef3f9364d3c4fbfc219b5e
Anna Garcia <annaegarcia@gmail.com>
2016-09-10 19:34:02 -0400

    add gethelp page

---
+++ components/GetHelp.js
@@ -0,0 +1,13 @@
+import React, { Component, PropTypes } from 'react';
+import { Text, View} from 'react-native';
+import Button from './Button'
+
+export default class GetHelp extends Component {
+  render() {
+    return (
+      <View>
+        <Text>'Get Help!'</Text>
+      </View>
+    )
+  }
+}
