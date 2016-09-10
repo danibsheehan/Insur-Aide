771c9b7a6822e65b535de2cf86e28bd322717a83
Stephanie Adams <adamsstephaniem@gmail.com>
2016-09-10 19:09:48 -0400

    created stylesheet from inline styles

--- components/Profile.js
+++ components/Profile.js
@@ -10,19 +10,30 @@ import {
 class MiniProfile extends Component {
 	render () {
 		return (
-			<View style={{flex:1, flexDirection: 'row',marginTop: 20, padding: 10}}>
-				<View style={{width: 80, height: 80, backgroundColor: "lightgray"}}>
+			<View style={styles.profileComp}>
+				<View style={styles.imgView}>
 					<Text >{this.props.profileData.imgUrl}</Text>
 				</View>
-				<View style={{width: 240, height: 80, paddingLeft: 10, paddingRight: 10}}>
-					<Text style={{fontSize: 20, fontWeight: 'bold'}}>{this.props.profileData.username}</Text>
-					<Text style={{fontSize: 20}}>{this.props.profileData.headline}</Text>
-					<Text style={{fontSize: 12}}>{this.props.profileData.categories.join(', ')}</Text>
-					<Text style={{fontSize: 12}}>{this.props.profileData.insurers.join(', ')}</Text>
+				<View style={styles.userDetails}>
+					<Text style={styles.name}>{this.props.profileData.username}</Text>
+					<Text style={styles.headline}>{this.props.profileData.headline}</Text>
+					<Text style={styles.text}>{this.props.profileData.categories.join(', ')}</Text>
+					<Text style={styles.text}>{this.props.profileData.insurers.join(', ')}</Text>
 				</View>
 			</View>
 		)
 	}
 }
+var styles =  StyleSheet.create({
+	profileComp: {flex:1, flexDirection: 'row',marginTop: 10, justifyContent:'space-around'},
+	imgView: {width: 100, height: 100, backgroundColor: "lightgray", justifyContent: 'center', alignItems: 'center'},
+	userDetails: {width: 250, height: 100, padding: 5},
+	name: {fontSize: 20, fontWeight: 'bold'},
+	headline: {fontSize: 16, marginVertical: 5},
+	text: {fontSize: 12},
+	separator: {border}
+
+})
+
 
 export default MiniProfile
