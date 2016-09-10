0f6bf07ff649c44c2865246e5fa13ff1cec15aa3
Svinci131 <svinci131@gmail.com>
2016-09-10 19:58:58 -0400

    made an external stylesheet

---
+++ styles/index.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+var styles = StyleSheet.create({
+    text: {
+        color: '#FF1744',
+    },
+    inputBox: {
+        borderColor: '#90A4AE',
+        borderWidth: 1,
+        padding: 2,
+        borderRadius: 5,
+        width: 300,
+        height: 40
+    }
+});
+
+export { styles }
