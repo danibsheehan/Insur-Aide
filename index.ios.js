0eeaad825dab81bf2019df8d663596ddc7fa125f
Anna Garcia <annaegarcia@gmail.com>
2016-09-10 18:47:30 -0400

    set up home navigation

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,104 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import React, { Component, PropTypes } from 'react';
+import { Navigator, Text, View, AppRegistry } from 'react-native';
+import HomeScene from './components/HomeScene'
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+export default class SimpleNavigationApp extends Component {
+  _renderScene(route, navigator){
+    var globalNavigatorProps = {navigator}
 
-class ReactApp extends Component {
+    switch(route.ident) {
+      case "Home":
+        return ( <View style={{marginTop: 50}}>
+            <Text style={{textAlign: 'center'}}>Penetrating the Bureaucracy</Text>
+            <HomeScene {...globalNavigatorProps}
+              // title={route.title}
+              // Function to call when a new scene should be displayed           
+              onForward={ () => {    
+                const nextIndex = route.index + 1;
+                navigator.push({
+                  title: 'Scene ' + nextIndex,
+                  index: nextIndex,
+                });
+              }}
+              // Function to call to go back to the previous scene
+              onBack={() => {
+                if (route.index > 0) {
+                  navigator.pop();
+                }
+              }}
+            />
+            </View>
+          )
+      default:
+        return <Text>'!!Route Error!!'</Text>
+    }
+
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
+      <Navigator
+        initialRoute={{ ident: 'Home' }}
+        renderScene={this._renderScene}
+      />
+    )
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+AppRegistry.registerComponent('ReactApp', () => SimpleNavigationApp);
 
-AppRegistry.registerComponent('ReactApp', () => ReactApp);
+// import React, { Component } from 'react';
+// import {
+//   AppRegistry,
+//   StyleSheet,
+//   Text,
+//   View
+// } from 'react-native';
+
+// import Button from './components/Button'
+
+// class ReactApp extends Component {
+
+//   _handlePress() {
+//     console.log('Pressed!');
+//   }
+
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Button press={this._handlePress} text={'This is a Button!'}/>
+//         <Text style={styles.welcome}>
+//           Welcome to React Native!
+//         </Text>
+//         <Text style={styles.instructions}>
+//           To get started, edit index.ios.js
+//         </Text>
+//         <Text style={styles.instructions}>
+//           Press Cmd+R to reload,{'\n'}
+//           Cmd+D or shake for dev menu
+//         </Text>
+//       </View>
+//     );
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: '#F5FCFF',
+//   },
+//   welcome: {
+//     fontSize: 20,
+//     textAlign: 'center',
+//     margin: 10,
+//   },
+//   instructions: {
+//     textAlign: 'center',
+//     color: '#333333',
+//     marginBottom: 5,
+//   },
+// });
+
+// AppRegistry.registerComponent('ReactApp', () => ReactApp);
