f3ad545591fdbc68285ff236e28f2de0f4c52002
Anna Garcia <annaegarcia@gmail.com>
2016-09-11 11:26:25 -0400

    fixed imports on profilelist

--- components/ProfileDetail.js
+++ components/ProfileDetail.js
@@ -1,8 +1,8 @@
-import React, { Component, PropTypes } from 'react';
-import { AppRegistry, Text, TextInput, View, TouchableOpacity } from 'react-native';
-import { styles } from '../styles'
-import ResultList from './components/ResultList'
-import Button from './components/Button'
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import TextArea from './TextArea'
+import MiniProfile from './MiniProfile'
+import Button from './Button'
 
 export default class ProfileDetail extends Component {
 	constructor(props) {
@@ -15,15 +15,14 @@ export default class ProfileDetail extends Component {
 		})
 	}
 	render() {
-		return {
-			//user's image, username, and summary component
-			<ResultList></ResultList>
-			//user's story description
+		return (
+					<View>
+			<MiniProfile></MiniProfile>
 			<View>
 				<TextArea>{this.props.story}</TextArea>
 			</View>
-			//button to message user
 			<Button text="Message Me" press={this.goToMessage.bind(this)}></Button>
-		}
+			</View>
+		)
 	}
 }
